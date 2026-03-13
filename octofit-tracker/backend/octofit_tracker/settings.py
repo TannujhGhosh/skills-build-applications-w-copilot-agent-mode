acc9bc1dfb775fec5165e69635c63eff5549b06e
Tannujh_Ghosh <104486135+TannujhGhosh@users.noreply.github.com>
2026-03-13 20:32:16 +0000

    modified:   octofit-tracker/backend/octofit_tracker/settings.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djongo',
+    'octofit_tracker',
+    'corsheaders',
 ]
     'rest_framework',
     'djongo',
@@ -50,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
